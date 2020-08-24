31f485c2d060296000bc5387c3ad4a0dba0f8c86
Hans Kristian <hanskristianom@gmail.com>
2020-08-24 13:54:34 +0200

    Test design for site

--- index.php
+++ index.php
@@ -1,9 +1,26 @@
-<html>
-<h1> TEST SIDE</h1>
-<p> Dette er godt </p>
-
+<!DOCTYPE html>
+<html lang="no" dir="ltr">
+<head>
+	<meta name="viewport" content="width=device-width">
+	<link rel="stylesheet" href="style/global.css">
+	<title>PRG1000 | Studentassistent</title>
+</head>
+<body>
+<header>
+	<section>
+		<div class="branding">
+			<h3 class="name">PRG1000 Hjem</h3>
+		</div>
+		
+		<nav>
+			<ul>
+				<li><a href="">Hjem</a></li>
+				<li><a href="">Spørsmål</a></li>
+				<li><a href="">FAQ</a></li>
+				<li><a href="">Om oss</a></li>
+			</ul>
+		</nav>
+	</section>
+</header>
+</body>
 </html>
-
-<?php
-    echo "hello world";
-    ?>
