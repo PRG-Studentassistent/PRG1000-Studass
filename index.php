64c03d53fde85ec08a035e7b7a58896ccebf4324
Hans Kristian <hanskristianom@gmail.com>
2020-08-24 21:33:55 +0200

    Testing design.

--- index.php
+++ index.php
@@ -7,40 +7,38 @@
 </head>
 <body>
 <header>
-	<section>
-		<div class="branding">
-			<h3 class="name">PRG1000 Hjem</h3>
-		</div>
-		<nav>
-			<ul>
-				<li>
-					<div>
-						<a href="">Hjem</a>
+	<div class="branding">
+		<h3 class="name">PRG1000 Hjem</h3>
+	</div>
+	<nav class="navbar">
+		<ul>
+			<li>
+				<div>
+					<a href="">Hjem</a>
+				</div>
+			</li>
+			<li>
+				<div>
+					<a href="">FAQ</a>
+				</div>
+			</li>
+			<li>
+				<div>
+					<a href="">Utfordring</a>
+				</div>
+			</li>
+			<li>
+				<div class="dropdown">
+					<button class="dropbtn">Kapitler</button>
+					<div class="dropdown-content">
+						<a href="#">Link 1</a>
+						<a href="#">Link 2</a>
+						<a href="#">Link 3</a>
 					</div>
-				</li>
-				<li>
-					<div>
-						<a href="">FAQ</a>
-					</div>
-				</li>
-				<li>
-					<div>
-						<a href="">Utfordring</a>
-					</div>
-				</li>
-				<li>
-					<div class="dropdown">
-						<button class="dropbtn">Kapitler</button>
-						<div class="dropdown-content">
-							<a href="#">Link 1</a>
-							<a href="#">Link 2</a>
-							<a href="#">Link 3</a>
-						</div>
-					</div>
-				</li>
-			</ul>
-		</nav>
-	</section>
+				</div>
+			</li>
+		</ul>
+	</nav>
 </header>
 
 </body>
