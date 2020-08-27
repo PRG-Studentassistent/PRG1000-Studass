8882e5ca6c28050c873af69cbffcc90f1a9ea633
Hans Kristian <hanskristianom@gmail.com>
2020-08-27 19:28:28 +0200

    test

--- access/database.php
+++ access/database.php
@@ -6,7 +6,6 @@ $username = "Hans";
 $password = "23011998";
 $dbname = "prg1000";
 
-global $db;
 $db = mysqli_connect($servername, $username, $password, $dbname);
 if (!$db) {
 	die("Connection failed: " . mysqli_connect_error());
